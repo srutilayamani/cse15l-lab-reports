d84b533100aaf6dc2f51c61ed6d89bc84e96fd04
srutilayamani <130116697+srutilayamani@users.noreply.github.com>
2023-04-24 17:53:18 -0700

    Update StringServer.java

--- labreport2/StringServer.java
+++ labreport2/StringServer.java
@@ -1,1 +1,43 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
 
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    ArrayList<String> arr = new ArrayList<String>();
+    public String handleRequest(URI url) {
+      
+        if (url.getPath().equals("/add-message")) {
+            return String.format("Sruti's number: %d", num);
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    arr.add(parameters[1]);
+                    display = "";
+                    for(String s : display){
+                      display += s + "/n"
+                    }
+                    return display;
+                }
+            }
+            return "Message not added! Please add a message to the end of the URL";
+        }
+    }
+}
+
+class NumberServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
